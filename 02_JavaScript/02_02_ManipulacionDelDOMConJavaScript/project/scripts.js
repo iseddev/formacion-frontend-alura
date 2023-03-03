7daab4a0c1a6c84a3e2ed03227f5b077911c969e
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2023-03-02 20:19:22 -0600

    Complete class 03

--- 02_JavaScript/02_02_ManipulacionDelDOMConJavaScript/project/scripts.js
+++ 02_JavaScript/02_02_ManipulacionDelDOMConJavaScript/project/scripts.js
@@ -20,8 +20,37 @@ const createTask = e => {
   console.log(dataFormInput)
 
   // Ahora asignamos a una variable el contenido (texto) que el usuario escribió:
-  let userTask = dataFormInput.value
-  console.log(userTask)
+  let taskContent = dataFormInput.value
+  console.log(taskContent)
+
+  // Ahora "limpiamos" el contenido del input, o más bien, cambiamos el contenido por un string vacío
+  dataFormInput.value = ""
+
+  // Capturamos el elemento "li" para poder agreagar la tarea escrita por el usuario a la lista de tareas
+  const taskList = d.querySelector("[data-task-list]")
+  console.log(taskList)
+
+  // Creamos el elemento "li" para poder agreagar la tarea escrita por el usuario a la lista de tareas
+  const taskItem = d.createElement("li")
+  // Agregamos la clase que dará los estilos requeridos
+  taskItem.classList.add("card")
+  console.log(taskItem)
+
+  // Definimos el "template" o plantilla de cada elemento li que se irá agregando a la lista de tareas
+  const templateTask = `
+    <div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${taskContent}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>
+  `
+  console.log(templateTask)
+
+  // Agregamos al nodo de nuestro li la estructura con la tarea generada
+  taskItem.innerHTML = templateTask
+  
+  // Agregamos cada tarea creada por el usuario al elemento ul
+  taskList.appendChild(taskItem)
 }
 
 // Inicializar un "listener" que esté "escuchando" un evento, en este caso, necesitamos escuchar el momento en que el usuario haga "click" sobre el botón para crear una nueva tarea, esto lo logramos con el método/función .addEventListener(), de la siguiente forma:
